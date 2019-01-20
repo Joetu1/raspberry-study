86133413e31a8a46e2bfe89258732bf900dbe20c
Joetu <tuweidongg@gmail.com>
2019-01-20 10:27:08 +0800

    初始化提交

---
+++ hello-world/blink/blink.c
@@ -0,0 +1,17 @@
+
+#include <wiringPi.h>
+
+int main(int argc, char const *argv[])
+{
+    wiringPiSetup();
+    pinMode(0, OUTPUT);
+    for (;;)
+    {
+        digitalWrite(0, HIGH);
+        delay(500);
+        digitalWrite(0, LOW);
+        delay(500);
+    }
+
+    return 0;
+}
---
+++ hello-world/readme.md
@@ -0,0 +1,13 @@
+## hello-world
+
+### 登录pi
+ssh pi@192.168.1.106
+
+### gcc
+
+- 生成可执行文件
+    ~~~
+    gcc hello.c -o hello
+    ~~~
+- 执行可执行文件
+    ./hello
